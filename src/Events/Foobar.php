46fa7c77510c6a551511406eb6542fc0264c4a93
boldpilot <ersoy.pembe@gmail.com>
2018-02-14 13:55:43 +0300

    invalid pr

---
+++ src/Events/Foobar.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace ErsoyInsider\NewrelicCustomEvent\Events;
+
+/**
+ * Class Foobar
+ * @package ErsoyInsider\NewrelicCustomEvent\Events
+ */
+class Foobar{
+
+
+    /**
+     * Foobar constructor.
+     */
+    public function __construct()
+    {}
+
+    public function invalid_method()
+    {
+
+    }
+
+    public function AnotherInvalidMethod(){
+
+    }
+}
